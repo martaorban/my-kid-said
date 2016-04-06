fa596167f9f8e1fc3bb8c278ce8acc1efc156d58
Marta Orban <marta.orban@gmail.com>
2016-04-05 22:03:38 -0700

    added sample data-list

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -1,14 +1,32 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  angular
-      .module('mksApp')
-      .controller('DashboardController', DashboardController);
+    angular
+            .module('mksApp')
+            .controller('DashboardController', DashboardController);
 
-  DashboardController.$inject = [];
+    DashboardController.$inject = [];
 
-  function DashboardController() {
-    var vm = this;
+    function DashboardController() {
+        var vm = this;
 
-  }
+        vm.latestQuote = [
+            {
+                "quote": "Hogy lett ebből sírás?",
+                "kidName": "Zsófi",
+                "date": "2015-02-11"
+            },
+            {
+                "quote": "Mindjárt is reggel lesz.",
+                "kidName": "Ábel",
+                "date": "2015-02-13"
+            },
+            {
+                "quote": "Ez egy tetej.",
+                "kidName": "Zsófi",
+                "date": "2015-02-22"
+            }
+        ];
+
+    }
 })();
